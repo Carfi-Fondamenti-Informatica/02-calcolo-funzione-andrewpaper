dae085e98fe75f1090d0b677ac0e236031d5b4e0
andrewpaper <118001300+andrewpaper@users.noreply.github.com>
2022-11-14 16:25:52 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,11 +3,11 @@ using namespace std;
 int main() {
 float a=0, b=0, x=0, y=0, z=0;  
 cin >>a, cin >>b, cin>>x, cin>>y;  
-if ((x<0) && (y>0) {   
+if ((x<0)&&(y>0)) {   
 z=(a*x)-(b*y);
 cout << z << endl;   
 }else if ((x>=0)&&(y<=0)) {   
-z=(a*x*x)-(b*y);
+z=(a*x*x)-(b*y*);
 cout << z << endl;
 }else{
 z=(a*x)+(b*y*y);
